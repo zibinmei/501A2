7b5b132611e55d14da35652e760c91f7619a70b1
zibinmei <zibinmei@outlook.com>
2017-10-26 16:06:15 -0600

    working on inspectionQueue

--- Inspector.java
+++ Inspector.java
@@ -1,61 +1,94 @@
 import java.lang.reflect.*;
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class Inspector {
-	Object object =  null;
-	Class classObject =null;
-	public void inspect(Object obj,boolean recursive) {
-		try {
-			object = obj;
-			classObject= obj.getClass();
-			
-			
-			System.out.println("Name of declaring class: "+classObject.getDeclaringClass().getName());
-			System.out.print("Name of immediate super class: "+ classObject.getSuperclass().getName());
-			
-			Class[] interfaces =classObject.getInterfaces();
-			System.out.println("Name of interface the class inplemented: ");
-			for (Class i : interfaces) {
-				System.out.println("\t"+i.getName());	
-			}
-			
-			Method[] methods = classObject.getDeclaredMethods();
-			System.out.println("Method: ");
-			for (Method m : methods) {
-				System.out.println("\t<"+m.getName()+">");
-				System.out.println("\texception thrown: "+m.getExceptionTypes().toString());
-				System.out.println("\tparameter types: "+m.getParameterTypes().toString());
-				System.out.println("\treturn type: "+m.getReturnType().toString());
-				System.out.println("\tmodifiers: "+m.getModifiers());
-			}
-			
-			Constructor[] construtors= classObject.getConstructors();
-			System.out.println("Constructors: ");
-			for (Constructor c : construtors) {
-				System.out.println("\t<"+c.getName()+">");
-				System.out.println("\tparameter types: "+c.getParameterTypes().toString());
-				System.out.println("\tmodifiers: "+c.getModifiers());
-				
-			}
-			
-			Field[] fields= classObject.getFields();
-			System.out.println("Fields: ");
-			for (Field f: fields) {
-				System.out.println("\t<"+f.getName()+">");
-				System.out.println("\ttype: "+f.getType().toString());
-				System.out.println("\tmodifiers: "+f.getModifiers());
-				if (f.getType().isPrimitive()) {
-					System.out.println("\tvalue: "+f.get(object).toString());
-				}
-				else if (recursive == false ){
-					System.out.println("\treference value: "+f.get(object).getClass().getName()+", "+f.get(object).getClass().hashCode());
-					
-				}
-			}
-				
+	static List<Class> inspected = new ArrayList<>();
+	private Object object =  null;
+	private Class classObject =null;
+	public void inspect(Object obj,boolean recursive) throws IllegalArgumentException, IllegalAccessException {
+		object = obj;
+		classObject= obj.getClass();
+		inspected.add(classObject);
+		
+		System.out.println("Name of declaring class: "+classObject.getName());
+		System.out.println("Name of immediate super class: "+ classObject.getSuperclass().getName());
+		
+		//print out all interfaces
+		Class[] interfaces =classObject.getInterfaces();
+		System.out.println("Name of interface the class implemented: ");
+		for (Class i : interfaces) {
+			System.out.println("\t"+i.getName());	
 		}
-		catch(Exception e) {}
+		
+		//print out all method
+		Method[] methods = classObject.getDeclaredMethods();
+		System.out.println("Method: ");
+		for (Method m : methods) {
+			System.out.println("\t<"+m.getName()+">");
+			System.out.print("\t\texception thrown: ");
+			arrayprinter(m.getExceptionTypes());
+			System.out.print("\t\tparameter types: ");
+			arrayprinter(m.getParameterTypes());
+			System.out.println("\t\treturn type: "+m.getReturnType().getSimpleName());
+			System.out.println("\t\tmodifiers: "+Modifier.toString(m.getModifiers()));
+		}
+		
+		//print out all constructor
+		Constructor[] construtors= classObject.getConstructors();
+		System.out.println("Constructors: ");
+		for (Constructor c : construtors) {
+			System.out.println("\t<"+c.getName()+">");
+			System.out.print("\t\tparameter types: ");
+			arrayprinter(c.getParameterTypes());
+			System.out.println("\t\tmodifiers: "+Modifier.toString(c.getModifiers()));
+			
+		}
+		//print out all Fields
+		Field[] fields= classObject.getFields();
+		System.out.println("Fields: ");
+		for (Field f: fields) {
+			System.out.println("\t<"+f.getName()+">");
+			System.out.println("\t\ttype: "+f.getType().getSimpleName());
+			System.out.println("\t\tmodifiers: "+Modifier.toString(f.getModifiers()));
+			if (f.getType().isPrimitive()) {
+				System.out.println("\t\tvalue: "+f.get(object).toString());
+			}
+			else if (recursive == false ){
+				System.out.println("\t\treference value: "+f.get(object).getClass().getSimpleName()+", "+f.get(object).getClass().hashCode());
+				
+			}
+			else if (recursive) {
+				
+			}
+		}
+			
+	
 		
 		
 	}
+	
+	private void arrayprinter(Class[] array) {
+		int current_index =0;
+		System.out.print("[");
+		for (Class e: array) {
+
+			System.out.print(e.getSimpleName());
+
+			if (current_index != (array.length-1) ) {
+				System.out.print(",");
+			}
+			
+
+				
+			else;
+			current_index++;
+		}
+		System.out.println("]");
+	}
+	
+	
 }
